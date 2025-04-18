317bdcbf872b7c0adc589428b65f0c238c039e45
Eduardo <89710660+eduardoferreira9@users.noreply.github.com>
2025-04-17 23:59:42 -0300

    add: navbar

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    // Import the head
+    <?php include 'includes/header.php'; ?>
+<body>
+    // Import the navbar
+    <?php include 'includes/navbar.php'; ?>
+
+    
+</body>
+</html>

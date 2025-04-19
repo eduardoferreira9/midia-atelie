4deae297302116409831c4852313e3dcdcb5843f
Eduardo <89710660+eduardoferreira9@users.noreply.github.com>
2025-04-18 23:30:26 -0300

    add: sections

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
     // Import the head
     <?php include 'includes/header.php'; ?>
 <body>
     // Import the navbar
     <?php include 'includes/navbar.php'; ?>
 
-    
+    // First section of site
+    <section>
+        <?php include 'includes/sections/first.php'; ?>
+    </section>
+
+    // Second section of site
+    <section>
+        <?php include 'includes/sections/second.php'; ?>
+    </section>
+
+    // Third section of site
+    <section>
+        <?php include 'includes/sections/third.php'; ?>
+    </section>
 </body>
 </html>
